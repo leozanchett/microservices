34e1fbabe46ccec3389ef16e28c5e8e26f26e7d4
Leo <45577227+leozanchett@users.noreply.github.com>
2024-01-16 14:24:40 -0300

    final project

--- linkedin learn/Building RESTful APIs with Flask/planetary_api/Project/app.py
+++ linkedin learn/Building RESTful APIs with Flask/planetary_api/Project/app.py
@@ -201,6 +201,35 @@ def add_planet():
         db.session.add(new_planet)
         db.session.commit()
         return jsonify(message='You added a planet'), 201
+    
+@app.route('/update_planet', methods=['PUT'])
+@jwt_required()
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name = request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        
+        db.session.commit()
+        return jsonify(message='You updated a planet'), 202
+    else:
+        return jsonify(message='That planet does not exist'), 404
+    
+@app.route('/remove_planet/<int:planet_id>', methods=['DELETE'])
+@jwt_required()
+def remove_planet(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message='You deleted a planet'), 202
+    else:
+        return jsonify(message='That planet does not exist'), 404
                 
 if __name__ == '__main__':
     print('Starting API')
